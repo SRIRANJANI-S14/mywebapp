21014f1ec60d6e039a670ff883c304cf868a2ca4
SRIRANJANI-S14 <83907447+SRIRANJANI-S14@users.noreply.github.com>
2021-05-10 08:21:53 +0530

    Add files via upload

---
+++ webapp.sql
@@ -0,0 +1,23 @@
+<?php
+$con=mysqli_connext("localhost","root","","registration");
+if(!$con){
+die('Connection failed');
+}
+if(isset($_POST['submit']))
+{
+$name=$_POST['name'];
+$course=$_POST['course'];
+$branch=$_POST['branch'];
+$email=$_POST['email'];
+$password=$_POST['password'];
+
+$sql_query="insert into user_details(name,course,branch,email,password)values(,$name','$course','$branch','$email','$password');";
+if(mysqli_query($con,$sql_query)){
+echo "success";
+}
+else{
+echo "error";
+}
+mysqli_close($con);
+}
+?>
